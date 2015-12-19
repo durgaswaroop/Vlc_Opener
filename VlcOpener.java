9e2f5e19ca9f98096c79c4f577bb206b6e84670f
DurgaSwaroop <durgaswaroop@gmail.com>
2015-12-19 23:41:57 +0530

    Vlc Opener edited

--- VlcOpener.java
+++ VlcOpener.java
@@ -4,7 +4,7 @@ public class VlcOpener {
 			try {
 					System.out.println("Vlc Opening!");
 					Runtime runTime = Runtime.getRuntime();
-					String []command = new String[2]; //= { "vlc" , "D:\\Awara - Title Song - Awara Hoon - Mukesh.mp4"};
+					String []command = new String[2]; 
 					command[0]="vlc";
 					command[1]=fileName;
 					Process process = runTime.exec(command);
